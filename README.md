afc321f89399f2a27fc80754d0a651314e2011f0
Dmitry Gorlov <wildeastengineer@gmail.com>
2022-11-13 11:31:41 +0400

    webpack assets loading

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# dictionary
+# Dictionary
+
+---
+
+# Resources
+
+- Dictionary icons created by [srip - Flaticon](https://www.flaticon.com/free-icons/dictionary)
+- Google Font [Nunito](https://fonts.google.com/specimen/Nunito?category=Sans+Serif&subset=cyrillic&preview.text=Hello%20%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82&preview.text_type=custom)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import styles from './style.css';
+import Book from './book.png';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  console.log('styles',styles);
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Book;
+
+  element.appendChild(myIcon);
+
+
+  console.log('Book',Book);
 
   return element;
 }
